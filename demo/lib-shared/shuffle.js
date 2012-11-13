088a85570016a3165dd7dbdae8ecce2c8de32de6
Ryan Roemer <ryan@loose-bits.com>
2012-11-13 06:09:43 -0500

    Various changes

--- demo/lib-shared/shuffle.js
+++ demo/lib-shared/shuffle.js
@@ -3,8 +3,9 @@ if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
 
+// Define wrapper gives us `require`
 define(function(require) {
-  // Looks like a normal "require".
+  // Looks like a normal `require` for Node.js.
   var _ = require('underscore');
 
   // Shuffle a list (space/comma separated).
