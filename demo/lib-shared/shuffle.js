556cb74951592e83a5faa68c798e81f05432f961
Ryan Roemer <ryan@loose-bits.com>
2012-11-12 20:49:22 -0500

    Add sample code slide

--- demo/lib-shared/shuffle.js
+++ demo/lib-shared/shuffle.js
@@ -11,7 +11,7 @@ define(function(require) {
   var shuffle = function (val) {
     val = val || "";
     return _.shuffle(val.split(/[\s,]+/));
-  }
+  };
 
   // Return value is exported.
   return shuffle;
