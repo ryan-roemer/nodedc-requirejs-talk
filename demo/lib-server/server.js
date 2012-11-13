944f6a33513f934c1e188563a5de254bfe63c0ed
Ryan Roemer <ryan@loose-bits.com>
2012-11-12 19:17:29 -0500

    Add shuffle.

--- demo/lib-server/server.js
+++ demo/lib-server/server.js
@@ -1,6 +1,7 @@
 // Simple server.
 var path = require('path'),
   express = require('express'),
+  shuffle = require('../lib-shared/shuffle'),
   app = express(),
   PUBLIC_DIR = path.join(__dirname, "../"),
   ADDR = "127.0.0.1",
@@ -8,9 +9,17 @@ var path = require('path'),
 
 app.configure(function() {
   app.use(express.methodOverride());
+  app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.favicon());
   app.use(express.static(PUBLIC_DIR));
+});
+
+app.get("/shuffle", function (req, res) {
+  var input = req.param('val'),
+    shuffled = shuffle(input);
+
+  return res.json(shuffled, 200);
 });
 
 app.listen(PORT, ADDR);
---
+++ demo/lib-shared/shuffle.js
@@ -0,0 +1,18 @@
+// Node.js hook boilerplate.
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+
+define(function(require) {
+  // Looks like a normal "require".
+  var _ = require('underscore');
+
+  // Shuffle a list (space/comma separated).
+  var shuffle = function (val) {
+    val = val || "";
+    return _.shuffle(val.split(/[\s,]+/));
+  }
+
+  // Return value is exported.
+  return shuffle;
+});
